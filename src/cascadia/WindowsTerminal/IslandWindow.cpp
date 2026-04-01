9cf2d9925481a743ebf70755e27f0359b96d45c6
yuhhhhhh56 <3537826110@qq.com>
2026-04-01 18:44:30 +0800

    Fix #20049: Add drag & drop support

--- src/cascadia/WindowsTerminal/IslandWindow.cpp
+++ src/cascadia/WindowsTerminal/IslandWindow.cpp
@@ -9,6 +9,7 @@
 #include <dwmapi.h>
 #include <TerminalThemeHelpers.h>
 #include <CoreWindow.h>
+#include <shellapi.h> 
 
 extern "C" IMAGE_DOS_HEADER __ImageBase;
 
@@ -387,7 +388,9 @@ void IslandWindow::Initialize()
 
     _rootGrid = winrt::Windows::UI::Xaml::Controls::Grid();
     _source.Content(_rootGrid);
-
+    DragAcceptFiles(_window.get(), TRUE);
+    // cspell:ignore MSGFLT_ADD WM_DROPFILES
+    ChangeWindowMessageFilter(WM_DROPFILES, MSGFLT_ADD);
     // initialize the taskbar object
     if (auto taskbar = wil::CoCreateInstanceNoThrow<ITaskbarList3>(CLSID_TaskbarList))
     {
